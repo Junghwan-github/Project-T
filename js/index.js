8920068b9cc74e59cc87ce34cfa191e92c940da9
JungHwan <j2studio.jungie2@gmail.com>
2023-12-06 14:33:58 +0900

    First commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/* 슬라이더 */
+$(document).ready(function () {
+  $(".slider_tab1").bxSlider();
+});
+
+$(document).ready(function () {
+  $(".slider_tab2").bxSlider();
+});
+
+$(document).ready(function () {
+  $(".slider_tab3").bxSlider();
+});
+$(document).ready(function () {
+  $(".slider_tab4").bxSlider();
+});
+$(document).ready(function () {
+  $(".slider_tab5").bxSlider();
+});
