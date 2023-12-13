1089f7de572778b0104fb543d0a8c7f0d80603c0
MinJi <mingmingddi@gmail.com>
2023-12-13 20:45:27 +0900

    joinForm UI, Validate Update

--- js/joinForm.js
+++ js/joinForm.js
@@ -1,61 +1,207 @@
-/* 이용약관 동의 시 -> 회원정보 기입페이지 연결 */
-const termSubmitBttn = document.querySelector(".termCheckBttn button");
+let id = document.getElementById("id");
+let emailDomain = document.getElementById("emailDomain");
+let selectedDomain = document.getElementById("domainSelect");
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+let uName = document.getElementById("uName");
+let nickName = document.getElementById("nickName");
+let phone = document.getElementById("phone");
+let birthDate = document.getElementById("birthDate");
 
-function checkAgreement(event) {
-  event.preventDefault();
-  let agreeCheckbox = document.getElementById("agreeCheckbox");
+let idFailMsg = document.querySelector(".idFailMsg");
+let idFailMsg2 = document.querySelector(".idFailMsg2");
+let idFailMsg3 = document.querySelector(".idFailMsg3");
+let pwdFailMsg = document.querySelector(".pwdFailMsg");
+let pwdFailMsg2 = document.querySelector(".pwdFailMsg2");
+let uNameFailMsg = document.querySelector(".uNameFailMsg");
+let nickNameFailMsg = document.querySelector(".nickNameFailMsg");
+let phoneFailMsg = document.querySelector(".phoneFailMsg");
+let birthFailMsg = document.querySelector(".birthFailMsg");
 
-  if (agreeCheckbox.checked) {
-    window.location.href = "/Project-T/joinForm.html";
-
-    return true;
-  } else {
-    alert("약관에 동의해야 합니다.");
-    return false;
-  }
+// 아이디 6자 이상 18자 이하
+function idLength(value) {
+  return value.length >= 6 && value.length <= 18;
+}
+// 아이디 영어 또는 숫자만
+function onlyNumberAndEnglish(str) {
+  return /^[A-Za-z0-9][A-Za-z0-9]*$/.test(str);
 }
 
-termSubmitBttn.addEventListener("click", checkAgreement);
-
-/* 이메일 유효성 검사 */
-
-function validateEmail(event) {
-  event.preventDefault();
-  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  let emailId = document.getElementById("emailId").value;
-  let emailDomain = document.getElementById("emailDomain").value;
-  let selectedDomain = document.getElementById("domainSelect").value;
-  let mail = emailId + "@" + emailDomain;
-
-  /*  idMsg = document.getElementById("idMsg");
-  errorText = document.createElement("span"); */
-
-  if (!emailId) {
-    /* errorText.innerText = "이메일아이디를 입력해주세요.";
-    errorText.appendChild(idMsg);
-    idMsg.classList.toggle("errorView"); */
-    alert("이메일아이디를 입력해주세요.");
-    document.querySelector("#emailId").focus();
-    return false;
-  }
-  if (!emailDomain) {
-    alert("도메인을 입력해주세요");
-    document.querySelector("#emailDomain").focus();
-    return false;
-  }
-
-  if (!emailRegex.test(mail)) {
-    alert("이메일을 형식에 맞게 입력해주세요.");
-    return false;
-  }
-
-  console.log(mail);
+// 이메일 형식 확인
+function emailRegex(value) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
 }
 
+/* 도메인 선택값 */
 function updateDomain() {
   let selectedDomain = document.getElementById("domainSelect").value;
   document.getElementById("emailDomain").value = selectedDomain;
 }
+
+// 비밀번호 8글자 이상 20자 이하, 영문, 숫자, 특수문자 사용
+function strongPassword(str) {
+  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/.test(
+    str
+  );
+}
+
+// 비밀번호 확인
+function isMatch(password1, password2) {
+  return password1 === password2;
+}
+
+// 이름 형식 확인 (한글, 영어 대소문자만 가능 특수기호,공백 불가)
+function validateUName(value) {
+  return /^[가-힣a-zA-Z]+$/.test(value);
+}
+
+// 닉네임 형식 확인 (한글, 숫자, 영어 대소문자만 가능 최소 2글자 최대8글자)
+function validateNickName(value) {
+  return /^[가-힣a-zA-Z0-9]{2,8}$/.test(value);
+}
+
+/* 휴대전화 번호 형식 확인 
+  010, 011, 016, 017, 018, 019 중 하나로 시작
+  하이픈(-)이 0 또는 1회 등장할 수 있음
+*/
+function validatePhone(value) {
+  return /^(01[016789])-?([0-9]{3,4})-?([0-9]{4})$/.test(value);
+}
+
+// 생년월일 형식 확인
+function validateBirth(value) {
+  return /^(19|20)\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])$/.test(value);
+}
+
+// 아이디 유효성 검사
+id.onkeyup = function () {
+  if (id.value.length !== 0) {
+    if (onlyNumberAndEnglish(id.value) === false) {
+      idFailMsg.classList.remove("hide");
+    } else if (idLength(id.value) === false) {
+      idFailMsg.classList.add("hide");
+      idFailMsg2.classList.remove("hide");
+      idFailMsg3.classList.add("hide");
+    } else {
+      idFailMsg.classList.add("hide");
+      idFailMsg2.classList.add("hide");
+      idFailMsg3.classList.add("hide");
+    }
+  } else {
+    idFailMsg.classList.add("hide");
+    idFailMsg2.classList.add("hide");
+    idFailMsg3.classList.add("hide");
+  }
+};
+
+// 비밀번호 유효성 검사
+password1.onkeyup = function () {
+  if (password1.value.length !== 0) {
+    if (strongPassword(password1.value)) {
+      pwdFailMsg.classList.add("hide");
+      pwdFailMsg2.classList.add("hide");
+    } else {
+      pwdFailMsg.classList.remove("hide");
+      pwdFailMsg2.classList.add("hide");
+    }
+  } else {
+    pwdFailMsg.classList.add("hide");
+    pwdFailMsg2.classList.add("hide");
+  }
+};
+
+// 비밀번호 일치 검사
+password2.onkeyup = function () {
+  if (password2.value.length !== 0) {
+    if (isMatch(password1.value, password2.value)) {
+      pwdFailMsg.classList.add("hide");
+      pwdFailMsg2.classList.add("hide");
+    } else {
+      pwdFailMsg2.classList.remove("hide");
+      pwdFailMsg.classList.add("hide");
+    }
+  } else {
+    pwdFailMsg.classList.add("hide");
+    pwdFailMsg2.classList.add("hide");
+  }
+};
+
+// 이메일 형식 검사
+emailDomain.onkeyup = function () {
+  let mail = id.value + "@" + emailDomain.value;
+  if (emailDomain.value.length !== 0) {
+    if (emailRegex(mail)) {
+      console.log(emailRegex(mail));
+      idFailMsg.classList.add("hide");
+      idFailMsg2.classList.add("hide");
+      idFailMsg3.classList.add("hide");
+    } else {
+      idFailMsg.classList.add("hide");
+      idFailMsg2.classList.add("hide");
+      idFailMsg3.classList.remove("hide");
+    }
+  } else {
+    idFailMsg.classList.add("hide");
+    idFailMsg2.classList.add("hide");
+    idFailMsg3.classList.add("hide");
+  }
+};
+
+// 이름  유효성 검사
+uName.onkeyup = function () {
+  if (uName.value.length !== 0) {
+    if (validateUName(uName.value)) {
+      uNameFailMsg.classList.add("hide");
+      nickNameFailMsg.classList.add("hide");
+    } else {
+      uNameFailMsg.classList.remove("hide");
+    }
+  } else {
+    uNameFailMsg.classList.add("hide");
+  }
+};
+
+// 닉네임 유효성 검사
+nickName.onkeyup = function () {
+  if (nickName.value.length !== 0) {
+    if (validateNickName(nickName.value)) {
+      nickNameFailMsg.classList.add("hide");
+      uNameFailMsg.classList.add("hide");
+    } else {
+      nickNameFailMsg.classList.remove("hide");
+    }
+  } else {
+    nickNameFailMsg.classList.add("hide");
+  }
+};
+
+// 휴대전화번호 형식 검사
+phone.onkeyup = function () {
+  if (phone.value.length !== 0) {
+    if (validatePhone(phone.value)) {
+      phoneFailMsg.classList.add("hide");
+      birthFailMsg.classList.add("hide");
+    } else {
+      phoneFailMsg.classList.remove("hide");
+    }
+  } else {
+    phoneFailMsg.classList.add("hide");
+  }
+};
+
+// 생년월일 형식 검사
+birthDate.onkeyup = function () {
+  if (birthDate.value.length !== 0) {
+    if (validateBirth(birthDate.value)) {
+      birthFailMsg.classList.add("hide");
+      phoneFailMsg.classList.add("hide");
+    } else {
+      birthFailMsg.classList.remove("hide");
+    }
+  } else {
+    birthFailMsg.classList.add("hide");
+  }
+};
 
 /* 주소 메서드 */
 function execDaumPostcode() {
